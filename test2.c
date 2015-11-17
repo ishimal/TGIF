bfdab9b090ea3ac2073ef39bcd10d879f873aa41
Ark <ark@ark-macbookpro-2.local>
2015-11-17 15:17:34 +0900

    いくつかの変数名の変更、点データの入力関数の完成

--- test2.c
+++ test2.c
@@ -1,9 +1,9 @@
 #include<stdio.h>
 #include<stdlib.h>
 int n = 2000, m = 2000;
-FILE *fp1,*fp2;
+FILE *fpIn1,*fpIn2,*fpOut;
 double point[2000][2000];
-int map1MinX,map1MinY;
+int map1MinX=0,map1MinY=0;
 //double afterDeterminPoints[7600][37000];
 
 typedef struct
@@ -14,28 +14,28 @@ typedef struct
 
 
 void initialize(){
-	fprintf(fp2,"#VRML V2.0 utf8\nNavigationInfo {\n\ttype [ \"EXAMINE\", \"ANY\" ]\n}\n\n");
+	fprintf(fpOut,"#VRML V2.0 utf8\nNavigationInfo {\n\ttype [ \"EXAMINE\", \"ANY\" ]\n}\n\n");
 }
 
 void draw(){
 	int i,j;
-	fprintf(fp2, "Shape {\n\tgeometry PointSet{\n\t\tcoord Coordinate{\n\t\t\tpoint[");
-	for(i=0;i<7600;i++){
-		for(j=0;j<37000;j++){
+	fprintf(fpOut, "Shape {\n\tgeometry PointSet{\n\t\tcoord Coordinate{\n\t\t\tpoint[");
+	for(i=0;i<n;i++){
+		for(j=0;j<m;j++){
 			if(point[i][j]>0){
-				fprintf(fp2, "%d %d %f", -1*i,-1*j,point[i][j]);
-				fprintf(fp2, ",");
+				fprintf(fpOut, "%d %d %f",  (i+map1MinX),(j+map1MinY),point[i][j]);
+				fprintf(fpOut, ",");
 			}
 		}
 	}
-	fprintf(fp2, "]\n\t\t}\n\t}\n}\n");
+	fprintf(fpOut, "]\n\t\t}\n\t}\n}\n");
 }
 
 void errorDraw(errorPoint errorPoints[],int k){
 	int i;
 	for (i=0;i<k;i++)
 	{
-		fprintf(fp2, "Transform{\n\ttranslation %f %f %f\n\tchildren[Shape {geometry Sphere{radius 1}\nappearance Appearance{material Material{diffuseColor 1 0 0}}}]\n\t}\n",-1*errorPoints[i].x,-1*errorPoints[i].y,0.0);
+		fprintf(fpOut, "Transform{\n\ttranslation %f %f %f\n\tchildren[Shape {geometry Sphere{radius 1}\nappearance Appearance{material Material{diffuseColor 1 0 0}}}]\n\t}\n",-1*errorPoints[i].x,-1*errorPoints[i].y,0.0);
 	}
 }
 
@@ -44,40 +44,47 @@ void inputPoints(void){
     double tmpx=0,tmpy=0,z=0;
     for(i=0;i<n;i++){
         for(j=0;j<m;j++){
-            point[i][j] = -1;
+            point[i][j] = -100;
         }
     }
     printf("done initialize\n");
-    ret = fscanf(fp1,"%lf %lf %lf",&tmpx,&tmpy,&z);
-    map1MinX=tmpx;
-    map1MinY=tmpy;
-    printf("%f %f \n",tmpx,tmpy);
-    while((ret = fscanf(fp1,"%lf %lf %lf",&tmpx,&tmpy,&z))!=EOF){
+    while((ret = fscanf(fpIn1,"%lf %lf %lf",&tmpx,&tmpy,&z)) != EOF){
+        if(tmpx < map1MinX) map1MinX=tmpx;
+        if(tmpy < map1MinY) map1MinY=tmpy;
+    }
+    while((ret = fscanf(fpIn2,"%lf %lf %lf",&tmpx,&tmpy,&z))!=EOF){
         x = tmpx - map1MinX;
-        y = map1MinY - tmpy;
+        y = tmpy - map1MinY;
         point[x][y] = z;
-        printf("%d %d\n",x,y); 
     }
 }
 
+
+
 int main(void){
-    if((fp1 = fopen("map1.dat","r")) == NULL){
-        printf("file1 open error!!\n");
+    if((fpIn1 = fopen("map1.dat","r")) == NULL){
+        printf("fpIn1 open error!!\n");
+        exit(-1);
+    }
+    if((fpIn2 = fopen("map1.dat","r")) == NULL){
+        printf("fpIn2 open error!!\n");
         exit(-1);
     }
     printf("done open\n");
-    if((fp2 = fopen("test.wrl","w")) == NULL){
-        printf("file2 open error!!\n");
+    if((fpOut = fopen("test.wrl","w")) == NULL){
+        printf("fpOut open error!!\n");
         exit(-1);
     }
     
     inputPoints();
+    printf("");
     initialize();
     draw();
     //errorDraw(errorPoints,k);
     
-    fclose(fp1);
-    fclose(fp2);
+    fclose(fpIn1);
+    fclose(fpIn2);
+    fclose(fpOut);
     
 	return 0;
 }
