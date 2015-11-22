f5ca1f2d6fba24570935f2fa90eccee82e4de8aa
ishimal <ishimal0318@gmail.com>
2015-11-22 14:47:00 +0900

    errorRestoreの追加、編集

--- test2.c
+++ test2.c
@@ -31,57 +31,6 @@ void draw(){
 	}
 	fprintf(fpOut, "]\n\t\t}\n\t}\n}\n");
 }
-
-void errorDraw(errorPoint errorPoints[],int k){
-	int i;
-	for (i=0;i<k;i++)
-	{
-		fprintf(fpOut, "Transform{\n\ttranslation %f %f %f\n\tchildren[Shape {geometry Sphere{radius 1}\nappearance Appearance{material Material{diffuseColor 1 0 0}}}]\n\t}\n",-1*errorPoints[i].x,-1*errorPoints[i].y,0.0);
-	}
-}
-
-<<<<<<< HEAD
-int main(void){
-	int n = 7600, m = 37000;
-	int i,j,k=0,ret,x,y;
-	double tmpx=0,tmpy=0;
-	double z=0;
-	errorPoint errorPoints[10000];
-	errorPoint ePoint;
-
-	point = malloc(sizeof(double *)*n);
-	base_point= malloc(sizeof(double *)*n*m);
-	for (i = 0; i < n; i++) {
-		point[i] = base_point + i*m;
-
-	}
-	for(i=0;i<7600;i++){
-		for(j=0;j<37000;j++){
-			point[i][j] = -1;
-		}
-	}
-
-	if((fp1 = fopen("53394640_dsm_1m.dat","r")) == NULL){
-		printf("file1 open error!!\n");
-		exit(-1);
-	}
-	if((fp2 = fopen("test.wrl","w")) == NULL){
-		printf("file2 open error!!\n");
-		exit(-1);
-	}
-
-	while((ret = fscanf("%lf %lf %lf",&tmpx,&tmpy,&z))!=EOF){
-		x = tmpx * -1;
-		y = tmpy * -1;
-		point[x][y] = z;
-		if (z==-9999.99)
-		{
-			ePoint.x = x;
-			ePoint.y = y;
-			errorPoints[k++] = ePoint;
-		}
-	}
-
 =======
 void inputPoints(void){
     int ret,x,y,i,j;
@@ -104,20 +53,24 @@ void inputPoints(void){
 }
 
 void errorRestore(void){
-    int i,j,s,t,cnt1,cnt2,sum=0;
+	int i, j, s, t, cnt1, cnt2, sum = 0, a[9] = {-100};
     double avg;
     for(j=0;j<n;j++){
         for(i=0;i<m;i++){
             if(point[i][j]<-1000){
-                sum=0;
-                cnt=0;
+				sum = 0;
+				cnt1 = 0;
+				cnt2 = 0;
                 if(point[i-1][j-1]!<-1000){/*エラー点で弾かれなかったやつは配列aに入れる*/
-                    sum+=point[i-1][j-1];
+					sum += point[i - 1][j - 1];
                     cnt1++;
+					a[cnt1] = point[i - 1][j - 1];
                 }
+				
                 if(point[i-1][j]!<-1000){
                     sum+=point[i-1][j];
                     cnt1++;
+					a[cnt1] = point[i - 1][j];
                 }
                 if(point[i-1][j+1]!<-1000){
                     sum+=point[i-1][j+1];
